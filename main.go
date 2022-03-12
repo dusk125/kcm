1a282c890b4ac78eeaba462bc3e4e2bed723ff1d
Allen Ray <alray@redhat.com>
2022-03-11 20:57:40 -0500

    Adding configuration and fixing issues

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"io/fs"
 	"log"
 	"os"
@@ -14,9 +14,60 @@ import (
 )
 
 type fileInfo struct {
-	Name string
-	Time time.Time
+	Name     string
+	Dir      string
+	Time     time.Time
+	Lifespan uint
 }
+
+func (f fileInfo) Path() string {
+	return path.Join(f.Dir, f.Name)
+}
+
+func (f fileInfo) Delete() tea.Msg {
+	return os.Remove(f.Path())
+}
+
+func (f fileInfo) Expired() bool {
+	if f.Lifespan == 0 {
+		return true
+	}
+	return time.Now().After(f.Time.Add(time.Minute * 150))
+}
+
+type ConfigDir struct {
+	Dir        string
+	FileSuffix string
+	FileFormat string
+	Lifespan   uint // Number of minutes until this cluster expires (0 is never expire)
+}
+
+type Config struct {
+	WatchDirs      []ConfigDir
+	KubeconfigLink string
+}
+
+func (c *Config) replace(o string, s string) {
+	c.KubeconfigLink = strings.ReplaceAll(c.KubeconfigLink, o, s)
+	for i := range c.WatchDirs {
+		c.WatchDirs[i].Dir = strings.ReplaceAll(c.WatchDirs[i].Dir, o, s)
+	}
+}
+
+var (
+	DefaultConfig = Config{
+		WatchDirs: []ConfigDir{
+			{
+				Dir:        "$HOME/Downloads",
+				FileSuffix: ".kubeconfig.txt",
+				FileFormat: "cluster-bot-2006-01-02-150405.kubeconfig.txt",
+				Lifespan:   150,
+			},
+		},
+		KubeconfigLink: "$HOME/.cluster",
+	}
+	config Config
+)
 
 type clusterList []fileInfo
 type activeMsg string
@@ -28,33 +79,36 @@ type model struct {
 	active   string
 }
 
-func initialModel() model {
-	return model{
+func initialModel() *model {
+	return &model{
 		clusters: make(clusterList, 0),
 	}
 }
 
 func initialState() tea.Msg {
 	var (
-		err   error
-		files []fs.DirEntry
+		err      error
+		files    []fs.DirEntry
+		clusters = clusterList{}
 	)
-	if files, err = os.ReadDir("/home/alray/Downloads"); err != nil {
-		return err
-	}
 
-	clusters := clusterList{}
-	for _, file := range files {
-		if !file.IsDir() && strings.HasSuffix(file.Name(), ".kubeconfig.txt") {
-			info := fileInfo{
-				Name: file.Name(),
+	for _, conf := range config.WatchDirs {
+		if files, err = os.ReadDir(conf.Dir); err != nil {
+			return err
+		}
+
+		for _, file := range files {
+			if !file.IsDir() && strings.HasSuffix(file.Name(), conf.FileSuffix) {
+				info := fileInfo{
+					Dir:      conf.Dir,
+					Name:     file.Name(),
+					Lifespan: conf.Lifespan,
+				}
+				if info.Time, err = time.Parse(conf.FileFormat, info.Name); err != nil {
+					return err
+				}
+				clusters = append(clusters, info)
 			}
-			if i, err := file.Info(); err != nil {
-				return err
-			} else if info.Time, err = time.Parse("2006-01-02-150405", strings.TrimSuffix(strings.TrimPrefix(i.Name(), "cluster-bot-"), ".kubeconfig.txt")); err != nil {
-				return err
-			}
-			clusters = append(clusters, info)
 		}
 	}
 
@@ -66,17 +120,26 @@ func readActive() tea.Msg {
 		err    error
 		active string
 	)
-	if active, err = os.Readlink("/home/alray/.cluster"); err != nil {
-		return ""
+	if active, err = os.Readlink(config.KubeconfigLink); err != nil {
+		return activeMsg("")
 	}
 	return activeMsg(path.Base(active))
 }
 
-func (m model) Init() tea.Cmd {
+func rmActive() tea.Msg {
+	_ = os.Remove(config.KubeconfigLink)
+	return nil
+}
+
+func (m *model) addActive() tea.Msg {
+	return os.Symlink(m.clusters[m.cursor].Path(), config.KubeconfigLink)
+}
+
+func (m *model) Init() tea.Cmd {
 	return tea.Batch(initialState, readActive)
 }
 
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m *model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case clusterList:
 		m.clusters = msg
@@ -100,57 +163,98 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				m.cursor++
 			}
 		case "enter", " ":
-			if _, m.err = os.Stat("/home/alray/.cluster"); os.IsExist(m.err) {
-				if m.err = os.Remove("/home/alray/.cluster"); m.err != nil {
-					return m, nil
-				}
-			}
-			m.err = os.Symlink(path.Join("/home/alray/Downloads", m.clusters[m.cursor].Name), "/home/alray/.cluster")
-			return m, readActive
+			return m, tea.Sequentially(rmActive, m.addActive, tea.Quit)
 		case "r":
 			return m, initialState
 		case "d":
-			m.err = os.Remove(path.Join("/home/alray/Downloads", m.clusters[m.cursor].Name))
-			return m, initialState
+			return m, tea.Sequentially(m.clusters[m.cursor].Delete, initialState)
 		case "c":
 			if m.active != "" {
-				_ = os.Remove("/home/alray/.cluster")
-				return m, readActive
+				return m, tea.Sequentially(rmActive, readActive)
 			}
 		}
 	}
 	return m, nil
 }
 
-func (m model) View() string {
+func (m *model) View() string {
 	var s string
 	if m.err != nil {
 		s = m.err.Error()
 	} else {
 		s = "Select a cluster.\n\n"
 
-		if m.active != "" {
-			s += fmt.Sprintf("Currently Active:\n\t%s\n\n", m.active)
-		}
-
 		for i, cluster := range m.clusters {
 			cursor := " "
 			if m.cursor == i {
 				cursor = ">"
 			}
-			f := "%s %s"
-			if time.Now().After(cluster.Time.Add(time.Minute * 150)) {
-				f = "%s [EXPIRED] %s"
+			ss := []string{cursor}
+			if cluster.Name == m.active {
+				ss = append(ss, "[x]")
+			} else {
+				ss = append(ss, "[ ]")
 			}
-			s += fmt.Sprintf(f+"\n", cursor, cluster.Name)
+			if time.Now().After(cluster.Time.Add(time.Minute * 150)) {
+				ss = append(ss, "[EXPIRED]")
+			}
+			ss = append(ss, cluster.Name)
+			s += strings.Join(ss, " ") + "\n"
 		}
 	}
 
-	s += "\nPress r to refresh | Press d to delete | Press q to quit.\n"
+	s += "\nPress r to refresh | Press d to delete | Press c to clear active | Press q to quit.\n"
 	return s
 }
 
 func main() {
+	var (
+		err  error
+		home string
+	)
+
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	if home, err = os.UserHomeDir(); err != nil {
+		log.Fatalln(err)
+	}
+
+	userConf := path.Join(home, ".kcm")
+	if _, err = os.Stat(userConf); os.IsExist(err) {
+		var (
+			fi *os.File
+		)
+
+		if fi, err = os.Open(userConf); err != nil {
+			log.Fatalln(err)
+		}
+		defer fi.Close()
+
+		if err = json.NewDecoder(fi).Decode(&config); err != nil {
+			log.Fatalln(err)
+		}
+	} else {
+		var (
+			fi *os.File
+		)
+
+		if fi, err = os.Create(userConf); err != nil {
+			log.Fatalln(err)
+		}
+		defer fi.Close()
+
+		config = DefaultConfig
+		config.replace("$HOME", home)
+
+		if err = json.NewEncoder(fi).Encode(&config); err != nil {
+			log.Fatalln(err)
+		}
+	}
+
+	if os.Getenv("KUBECONFIG") == "" {
+		log.Fatalf("KUBECONFIG needs to be set:\n\texport KUBECONFIG=%v\n", config.KubeconfigLink)
+	}
+
 	p := tea.NewProgram(initialModel())
 	if err := p.Start(); err != nil {
 		log.Fatalln(err)
